18db7aeaecc1b9e77c593d71f2d040286581a2fc
daniel <prokopowiczdaniel@gmail.com>
2019-10-16 00:07:21 +0200

    my first version

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,24 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// place your code below
+if('.list--js') {
+  const list = document.querySelector('.list--js');
+    fetch('https://api.github.com/users/doneilprakapawecz/repos').
+    then(resp => resp.json())
+    .then(resp => {
+      const repos = resp;
+      for (const repo of repos) {
+        const {description,html_url, name} = repo;
+        list.innerHTML += `<li class="list__repo--element">
+             <a href="${html_url}"class="link"target="_blank"> ${name} </a>
+             <p class="paragrath">${description ? description: "brak opisu" }</p>
+         </li>`;
+      }
+    }).catch(err => {
+      console.log(err); 
+    })
+   
+} 
 
 
 console.log(`Hello world!`);
