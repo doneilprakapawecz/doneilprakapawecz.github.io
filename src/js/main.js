49ba98377a44ab8d6c37e20445e247b210ff3f8c
daniel <prokopowiczdaniel@gmail.com>
2019-11-24 21:22:09 +0100

    k__version

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,37 @@
+
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-const list = document.querySelector('.list--js');
-if('.list--js') {
-    fetch('https://api.github.com/users/doneilprakapawecz/repos').
-    then(resp => resp.json())
-    .then(resp => {
-      const repos = resp;
-      list.innerHTML="";
-      for (const repo of repos) {
-        const {description,html_url, name} = repo;
-        list.innerHTML+= `<li class="list__repo--element">
-             <a href="${html_url}"class="link"target="_blank">
-             <i class="icon-github-circled-alt2"></i><br>  ${name} </a>
-             <p class="paragrath">${description ? description: "brak opisu" }</p>
-         </li>`;
-      }
-    }).catch(err => {
-      console.log(err); 
-    })
-   
-} 
-
-
 console.log(`Hello world!`);
+
+const list = document.querySelector('.k__projects-list--js');
+
+fetch('https://api.github.com/users/doneilprakapawecz/repos?sort=updated&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+   const repos = resp; 
+   for (const repo of repos) {
+     const {description, homepage, html_url, name} = repo;
+     list.innerHTML += `
+      <li class="k__project">
+        <div class="k__project__container">
+          <img class="k__project__logo" src="assets/img/github.svg" alt="">
+          <h3 class="project__title">${name}</h3>
+          ${
+            description ? `<p class="k__project__description">${description}</p>` : 'No description 😔'
+          }
+        </div>
+        <div class="k__project__footer">
+          ${
+            homepage ? `<a class="k__project__link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>` : ''
+          }
+          <a class="k__project__link k__project__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer"  title="Source code: ${name}.">GitHub</a>
+        </div>
+      </li>
+     `;
+   }
+  })
+  .catch(err => {
+    console.log(err);
+  })
 
 
